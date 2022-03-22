14e401c7a2e1095fd79c1869967be5db5322ec6b
Les <lfleming@elbicon.com>
2022-03-22 13:29:41 +0000

    Spring Boot Template

---
+++ development/src/main/java/elbicon/com/development/DevelopmentApplication.java
@@ -0,0 +1,13 @@
+package elbicon.com.development;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevelopmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevelopmentApplication.class, args);
+	}
+
+}
---
+++ development/src/test/java/elbicon/com/development/DevelopmentApplicationTests.java
@@ -0,0 +1,13 @@
+package elbicon.com.development;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevelopmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
